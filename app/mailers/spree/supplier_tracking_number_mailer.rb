7ea0abede80fc5ec9f558de17307cb5d0bf91bd1
Julián Ortiz <julian.ortiz@web-experto.com.ar>
2019-02-26 16:01:38 -0300

    Add mail template

--- app/mailers/spree/supplier_tracking_number_mailer.rb
+++ app/mailers/spree/supplier_tracking_number_mailer.rb
@@ -2,6 +2,7 @@ module Spree
   class SupplierTrackingNumberMailer < BaseMailer
     def notify_supplier(pdf, order)
       attachments["#{order.number}.pdf"] = pdf
+      @order                             = order
 
       mail from:      from_address,
            reply_to:  from_address,
